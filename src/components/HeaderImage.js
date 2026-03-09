80f7d8ca33dac56a48668ba26980746c57877277
BenABryant <bemanatee@icloud.com>
2026-03-08 21:05:35 -0700

    decluttering

--- src/components/HeaderImage.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./HeaderImage.css";
-export default function HeaderImage() {
-    return (_jsx("div", { className: "header-image", children: _jsx("img", { src: "/header.jpeg", alt: "View of south pacific island from the water", className: "header-image__img" }) }));
-}
